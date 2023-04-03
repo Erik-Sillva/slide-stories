44513c093a1a7dfac74397d3755f44e63ad932bb
Erik Silva <104636199+Erik-Sillva@users.noreply.github.com>
2023-04-03 14:37:34 -0300

    Initial commit

---
+++ dist/Slide.js
@@ -0,0 +1,163 @@
+import Timeout from "./Timeout.js";
+const divSound = document.createElement('div');
+divSound.className = 'div-sound';
+const buttonSound = document.createElement('i');
+buttonSound.className = 'fa-solid fa-volume-xmark';
+divSound.appendChild(buttonSound);
+export default class Slide {
+    container;
+    element;
+    slides;
+    activeSound;
+    controls;
+    time;
+    index;
+    slide;
+    timeout;
+    pausedTimeout;
+    paused;
+    thumbItems;
+    thumb;
+    constructor(container, element, slides, controls, time = 5000) {
+        this.container = container;
+        this.element = element;
+        this.slides = slides;
+        this.activeSound = true;
+        this.controls = controls;
+        this.time = time;
+        this.timeout = null;
+        this.pausedTimeout = null;
+        this.index = localStorage.getItem('activeSlide') ? Number(localStorage.getItem('activeSlide')) : 0;
+        this.slide = this.slides[this.index];
+        this.paused = false;
+        this.thumbItems = null;
+        this.thumb = null;
+        this.init();
+    }
+    hide(el) {
+        el.classList.remove('active');
+        if (el instanceof HTMLVideoElement) {
+            el.currentTime = 0;
+            el.pause();
+        }
+    }
+    soundVideo(video) {
+        if (buttonSound.classList.contains('fa-volume-xmark')) {
+            buttonSound.classList.remove('fa-volume-xmark');
+            buttonSound.classList.add('fa-volume-high');
+            this.activeSound = false;
+        }
+        else if (buttonSound.classList.contains('fa-volume-high')) {
+            buttonSound.classList.remove('fa-volume-high');
+            buttonSound.classList.add('fa-volume-xmark');
+            this.activeSound = true;
+        }
+        video.muted = this.activeSound;
+    }
+    show(index) {
+        this.index = index;
+        this.slide = this.slides[this.index];
+        localStorage.setItem('activeSlide', String(this.index));
+        if (this.thumbItems) {
+            this.thumb = this.thumbItems[this.index];
+            this.thumbItems.forEach(el => el.classList.remove('active'));
+            this.thumb.classList.add('active');
+        }
+        this.slides.forEach(el => this.hide(el));
+        this.slide.classList.add('active');
+        if (this.slide instanceof HTMLVideoElement) {
+            this.container.appendChild(divSound);
+            this.autoVideo(this.slide);
+        }
+        else {
+            this.auto(this.time);
+            if (this.container.appendChild(divSound)) {
+                this.container.removeChild(divSound);
+            }
+        }
+    }
+    autoVideo(video) {
+        video.muted = this.activeSound;
+        video.play();
+        let firstPlay = true;
+        video.addEventListener('playing', () => {
+            if (firstPlay)
+                this.auto(video.duration * 1000);
+            firstPlay = false;
+        });
+    }
+    auto(time) {
+        this.timeout?.clear();
+        this.timeout = new Timeout(() => this.next(), time);
+        if (this.thumb)
+            this.thumb.style.animationDuration = `${time}ms`;
+    }
+    prev() {
+        if (this.paused)
+            return;
+        const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+        this.show(prev);
+    }
+    next() {
+        if (this.paused)
+            return;
+        const next = (this.index + 1) < this.slides.length ? this.index + 1 : 0;
+        this.show(next);
+    }
+    pause() {
+        document.body.classList.add('paused');
+        this.pausedTimeout = new Timeout(() => {
+            this.timeout?.pause();
+            this.paused = true;
+            this.thumb?.classList.add('paused');
+            if (this.slide instanceof HTMLVideoElement) {
+                this.slide.pause();
+            }
+        }, 200);
+    }
+    continue() {
+        document.body.classList.remove('paused');
+        this.pausedTimeout?.clear();
+        if (this.paused) {
+            this.paused = false;
+            this.timeout?.continue();
+            this.thumb?.classList.remove('paused');
+            if (this.slide instanceof HTMLVideoElement) {
+                this.slide.play();
+            }
+        }
+    }
+    addControls() {
+        const prevButton = document.createElement('button');
+        const nextButton = document.createElement('button');
+        prevButton.innerText = 'Slide Anterior';
+        nextButton.innerText = 'Próximo Slide';
+        this.controls.appendChild(prevButton);
+        this.controls.appendChild(nextButton);
+        this.controls.addEventListener('pointerdown', () => this.pause());
+        document.addEventListener('pointerup', () => this.continue());
+        document.addEventListener('touchend', () => this.continue());
+        prevButton.addEventListener('pointerup', () => this.prev());
+        nextButton.addEventListener('pointerup', () => this.next());
+    }
+    addThumbItems() {
+        const thumbContainer = document.createElement('div');
+        thumbContainer.id = 'slide-thumb';
+        for (let i = 0; i < this.slides.length; i++) {
+            thumbContainer.innerHTML += `<span><span class="thumb-item"></span></span>`;
+        }
+        this.controls.appendChild(thumbContainer);
+        this.thumbItems = Array.from(document.querySelectorAll('.thumb-item'));
+    }
+    init() {
+        this.addControls();
+        this.addThumbItems();
+        this.show(this.index);
+        divSound.addEventListener('click', () => {
+            if (this.slide instanceof HTMLVideoElement) {
+                this.soundVideo(this.slide);
+            }
+        });
+    }
+}
+//# sourceMappingURL=Slide.js.map
